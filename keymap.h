088c47c7b565c88c3c7f98b530476d9e55cee68b
premek <premysl.vyhnal@gmail.com>
2018-03-12 03:13:39 +0100

    my layout

--- keymap.h
+++ keymap.h
@@ -1,32 +1,39 @@
 #ifndef _KEYMAP_H
 #define _KEYMAP_H
 
-#define KEY_NEXT        XK_Tab
+#define KEY_NEXT        XK_g
 #define KEY_NEW         XK_Return
-#define KEY_TOPLEFT     XK_y
-#define KEY_TOPRIGHT    XK_u
-#define KEY_BOTTOMLEFT  XK_b
-#define KEY_BOTTOMRIGHT XK_n
-#define KEY_LEFT        XK_h
-#define KEY_RIGHT       XK_l
-#define KEY_DOWN        XK_j
-#define KEY_UP          XK_k
+
+#define KEY_TOPLEFT     XK_u
+#define KEY_TOPRIGHT    XK_i
+#define KEY_BOTTOMLEFT  XK_m
+#define KEY_BOTTOMRIGHT XK_comma
+
+#define KEY_LEFT        XK_j
+#define KEY_RIGHT       XK_semicolon
+#define KEY_DOWN        XK_k
+#define KEY_UP          XK_l
+
 #define KEY_LOWER       XK_Insert
 #define KEY_ALTLOWER    XK_KP_Insert
-#define KEY_INFO        XK_i
-#define KEY_MAXVERT     XK_equal
-#define KEY_MAX         XK_x
-#define KEY_DOCK_TOGGLE XK_d
+
+#define KEY_INFO        XK_z
+#define KEY_MAXVERT     XK_x
+#define KEY_MAX         XK_c
+
+#define KEY_DOCK_TOGGLE XK_b
+
 #ifdef VWM
 # define KEY_FIX        XK_f
-# define KEY_PREVDESK   XK_Left
-# define KEY_NEXTDESK   XK_Right
-# define KEY_TOGGLEDESK XK_a
+# define KEY_PREVDESK   XK_e
+# define KEY_NEXTDESK   XK_d
+# define KEY_TOGGLEDESK XK_v
 #endif
+
 /* Mixtures of Ctrl, Alt an Escape are used for things like VMWare and
  * XFree86/Cygwin, so the KILL key is an option in the Makefile  */
 #ifndef KEY_KILL
-# define KEY_KILL       XK_Escape
+# define KEY_KILL       XK_a
 #endif
 
 #endif
